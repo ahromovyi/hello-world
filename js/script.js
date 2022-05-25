c93e8432686ec3bc09c643663d1e8f3a1ffa2245
Andrii <hromovyi.andrii@gmail.com>
2022-05-26 02:00:41 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+alert('Hello!');
+
+/* console.log(1);
+
+const obj = {
+    name: 'John',
+    age: 45,
+    isMarried: false,
+    notMarried: true
+}; */
+
+// console.log(obj.notMarried);
+// console.log(obj['notMarried']);
